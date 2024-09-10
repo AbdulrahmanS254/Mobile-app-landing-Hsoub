ab0d62cbcc7b8eaa95cccd584f2170f3933b1883
Abdulrahman Shawky <hawk18922@gmail.com>
2024-09-10 14:35:37 +0300

    exam commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+(() => {
+   "use strict";
+
+   const forms = document.querySelectorAll(".needs-validation");
+
+   Array.from(forms).forEach((form) => {
+      form.addEventListener(
+         "submit",
+         (event) => {
+            if (!form.checkValidity()) {
+               event.preventDefault();
+               event.stopPropagation();
+            }
+
+            form.classList.add("was-validated");
+         },
+         false
+      );
+   });
+})();
